b1cf77f18a56d634a2fab5663c75d2308efb71ce
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-09-15 15:11:41 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh -l
 
-echo "Hello $2"
-profile="dotnet build"
-echo "::set-output name=profile::$profile"
+if ["$a" = 'dotnet']
+then
+  echo "hello"
+else
+  echo "Nothing"
+fi
+
+#echo "Hello $2"
+#profile="dotnet build"
+#echo "::set-output name=profile::$profile"
+
 #time=$(date)
 #echo "::set-output name=time::$time"
